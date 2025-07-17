63a8644ec36941913a4fb9fce65cb9e36371636e
chathurajanithya <it21263880@my.sliit.lk>
2025-07-17 20:42:31 +0530

    Add task data structure and initial tasks to JSON file

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+type Status = "To Do" | "In Progress" | "Approved" | "Reject";
+
+type Priority = "low" | "medium" | "high";
+
+type Assignee = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
+export interface Task {
+  id: string;
+  title: string;
+  category: string;
+  status: Status;
+  assignees: Assignee[];
+  attachments?: number;
+  comments?: number;
+  dueDate?: string;
+  reports?: number;
+  hasStream?: boolean;
+  hasPreview?: boolean;
+  hasGroupCall?: boolean;
+  priority: Priority;
+}
