25fd34b75159b05d8d1c478c73e2b8754e1541c8
chathurajanithya <it21263880@my.sliit.lk>
2025-07-18 13:23:50 +0530

    feat: add Plausible analytics script to RootLayout for tracking user interactions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <script
+          defer
+          data-domain="main.d3oesffdu3pujr.amplifyapp.com"
+          src="https://plausible.io/js/script.js"
+        ></script>
         {children}
       </body>
     </html>
